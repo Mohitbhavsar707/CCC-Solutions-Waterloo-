d5a23f4f3de525c756dd3d7a4716bee429b91d12
Mohit Bhavsar <mohitbhavsar707@gmail.com>
2021-02-15 12:07:10 -0500

    Added comments

--- src/Problems2020/j1.java
+++ src/Problems2020/j1.java
@@ -6,10 +6,13 @@ public class j1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        // Receive the input (the three letters from the user)
         int S = in.nextInt();
         int M = in.nextInt();
         int L = in.nextInt();
 
+        // insert the input into the equation provided and check if the sum is greater than or equal to 10
+        //  if it is, then print "happy", else, "sad"
         if ((S + (2*M) + (3*L)) >= 10){
             System.out.println("happy");
         } else{
--- src/Problems2020/j2.java
+++ src/Problems2020/j2.java
@@ -8,19 +8,25 @@ public class j2 {
         
     
         Scanner input = new Scanner(System.in);
-        int P = input.nextInt();
-        int N = input.nextInt();
-        int R = input.nextInt();
 
-        int numInfectedPeople = N;
-        int numInfectedPeopleLastDay = N;
-        int days = 0;
+        // receive input from the user
+        int P = input.nextInt(); // will be used as the condition - more than or equal to P users
+        int N = input.nextInt(); // number of people with the disease on Day 0
+        int R = input.nextInt(); // other people infected
 
+        int numInfectedPeople = N; // number of infected people begins with N (Day 0), hence, store this value here
+        int days = 0; // to check the amount of days it takes
+
+        /* Loop
+               1 += 1* (5^1); = 6
+               6 += 1 * (5^2); = 31
+               31 += 1 * (5^3) = 156
+               156 += 1 * (5^4) = 781 (Limit has been reached on the fourth day) = exit loop */
+        
         while (numInfectedPeople <= P){
-            days += 1;
-            numInfectedPeople += numInfectedPeopleLastDay * R;
-            numInfectedPeopleLastDay *= R;
-
+            days++;
+            numInfectedPeople += N*(Math.pow(R, days));
+            System.out.println(numInfectedPeople);
         }
         System.out.println(days);
 
